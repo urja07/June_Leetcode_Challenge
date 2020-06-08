b62403d630e137a0f8a46980ba7cdf640cb0df38
Urja <urjathakur5@gmail.com>
2020-06-08 19:22:31 +0530

    Added day 8 code

---
+++ PowerOf2.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n>0)
+        {
+            if((n & (n-1))==0)
+                return true;
+            else
+                return false;
+        }
+        return false;
+    }
+};
