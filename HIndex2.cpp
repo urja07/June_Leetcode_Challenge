c8599c3f70f57bff2f9de8194986b1fcc8310e6b
Urja <urjathakur5@gmail.com>
2020-06-19 11:43:51 +0530

    Added day 18 code

---
+++ HIndex2.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        int n= citations.size();
+        int l=0, r=n-1;
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            if(citations[mid]<n-mid)
+                l=mid+1;
+            else
+                r=mid-1;
+        }
+        return  n-l;
+    }
+};
